cd796be337a5868ffa3a4a6938df23451ac5b7bb
admin <kanikashrivastava1999@gmail.com>
2020-08-30 18:17:07 +0530

    Updates

---
+++ precache-manifest.3d0067bda6a7a34b157b68b0613a2408.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6855dba7825ee9145666c08edb201927",
+    "url": "/Peaple-Activity-Tracker/index.html"
+  },
+  {
+    "revision": "95d14bdd2910a915d64c",
+    "url": "/Peaple-Activity-Tracker/static/css/main.6c85d1e9.chunk.css"
+  },
+  {
+    "revision": "c30880ef421f48c88766",
+    "url": "/Peaple-Activity-Tracker/static/js/2.c884acea.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Peaple-Activity-Tracker/static/js/2.c884acea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95d14bdd2910a915d64c",
+    "url": "/Peaple-Activity-Tracker/static/js/main.330d6648.chunk.js"
+  },
+  {
+    "revision": "914f265b4a1a4c124473",
+    "url": "/Peaple-Activity-Tracker/static/js/runtime-main.2a755757.js"
+  }
+]);
