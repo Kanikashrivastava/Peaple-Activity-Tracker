e4a6eeccaef9a365bde179beb96dda7612e24f79
admin <kanikashrivastava1999@gmail.com>
2020-08-30 20:55:13 +0530

    api added

--- src/components/landingPage/index.js
+++ src/components/landingPage/index.js
@@ -2,16 +2,41 @@ import React, { Component } from 'react';
 import { styles } from './style';
 import Header from '../Header';
 import UserCard from '../UserCard';
-import data from '../../API/Test.json';
+
+const URL = `https://run.mocky.io/v3/14bad0a1-9ed7-4b24-9be3-a727f835bfef`;
 
 class LandingPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userData: [],
+        }
+    }
+
+    componentDidMount() {
+        this.getUserData();
+    }
+
+    getUserData = async () => {
+        fetch(URL).then(response => response.json())
+            .then(data =>
+                this.setState({
+                    userData: data.members
+                }))
+    }
     render() {
-        const { members } = data;
+        const { userData } = this.state;
         return (
             <div>
                 <Header />
                 <div style={styles.cardContainer}>
-                    {members.map((member) => <UserCard user={member} key={member.id} />)}
+                    {userData.length ?
+                        userData.map((member) =>
+                            <UserCard user={member} key={member.id} />
+                        ) :
+                        <p style={styles.noResultText}>
+                            {'No Result found'}
+                        </p>}
                 </div>
             </div>
         )
--- src/components/landingPage/style.js
+++ src/components/landingPage/style.js
@@ -1,5 +1,9 @@
 export const styles = {
     cardContainer: {
         margin: 10,
+    },
+    noResultText: {
+        textAlign: 'center',
+        fontSize: '20px'
     }
 };
